084c3a00c55c464f797a67687d86e8f7a90fd934
Virginijus Magelinskas <virginijus@noia.network>
2020-06-26 12:40:40 +0000

    Add notification with details

--- get_stocks.py
+++ get_stocks.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import sys
+import os
+import notify2
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
@@ -16,12 +18,33 @@ ticker_stock = soup.find('h1',class_='float_lang_base_1 relativeAttr')
 actual = soup.find(id='fl_header_pair_lst')
 change = soup.find(id='fl_header_pair_chg')
 percent = soup.find(id='fl_header_pair_pch')
+notification_text = 'Actual: '+actual.text+'\n Change: '+change.text+'\n Percent change: '+percent.text
 
 if float(percent.text.strip("%")) < 0:
-    if ticker.text == None:
+    actual_text = '<span color=\'red\'>'+actual.text+'</span>'
+    change_text = '<span color=\'red\'>'+change.text+'</span>'
+    percent_text = '<span color=\'red\'>'+percent.text+'</span>'
+else:
+    actual_text = '<span color=\'#77e326\'>'+actual.text+'</span>'
+    change_text = '<span color=\'#77e326\'>'+change.text+'</span>'
+    percent_text = '<span color=\'#77e326\'>'+percent.text+'</span>'
+
+notification_text = 'Actual: '+actual_text+'\n'+'Change: '+change_text+'\n'+'Percent: '+percent_text
+
+if os.environ.get('BLOCK_BUTTON'):
+    if (os.environ['BLOCK_BUTTON'] == '1'):
+        n = notify2.init("Stocks and Futures")
+        notification = notify2.Notification(ticker.text, notification_text, "dialog-information")
+        notification.timeout = 3000
+        notification.show()
+
+if float(percent.text.strip("%")) < 0:
+    if ticker.text is None:
         print(ticker_stock.text.replace("&", "&amp;")+': '+'<span color=\'red\'>'+percent.text+'</span>')
     else:
         print(ticker.text.replace("&", "&amp;")+': '+'<span color=\'red\'>'+percent.text+'</span>')
 else:
     print(ticker.text.replace("&", "&amp;")+': '+'<span color=\'#77e326\'>'+percent.text+'</span>')
-    
+
+
+
